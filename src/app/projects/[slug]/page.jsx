18e21003a01abad3b244154857aefdc6e2c3215b
Leonel Castro <barckstar.lml@gmail.com>
2024-06-06 20:06:23 -0600

    Fixed bugs

--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -1,6 +1,4 @@
 "use client";
-import { useParams } from "next/navigation";
-import data from "@/lib/data.json";
 import PostContent from "@/components/postContent";
 
 export default function Post() {
--- src/app/specialties/[slug]/page.jsx
+++ src/app/specialties/[slug]/page.jsx
@@ -1,8 +1,5 @@
 "use client";
-import { useParams } from "next/navigation";
-import data from "@/lib/data.json";
 import PostContent from "@/components/postContent";
-import FillNav from "@/components/FillNav";
 
 export default function Post() {
   return (
