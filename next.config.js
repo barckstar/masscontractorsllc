3a38876b7c40912f9422719ae9bee9480ecfe937
Leonel Castro <barckstar.lml@gmail.com>
2024-08-08 20:32:06 -0600

    remove CSP

--- next.config.js
+++ next.config.js
@@ -1,37 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    images: {
-      domains: [
-        'images.pexels.com',
-        'dummyimage.com'
-      ]
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: `
-                default-src 'none';
-                script-src 'self' 'nonce-random123' https://trusted.cdn.com;
-                style-src 'self' 'unsafe-inline';
-                font-src 'self' https://fonts.example.com; // Permitir fuentes
-                img-src 'self' data: https://images.example.com;
-                connect-src 'self' https://api.example.com;
-                media-src 'self' https://media.example.com; // Permitir medios
-                frame-src https://www.example.com; // Permitir enmarcar ciertos sitios
-                frame-ancestors 'none';
-                base-uri 'self';
-                form-action 'self';
-              `.replace(/\s+/g, ' ').trim(),
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+    images:{
+        domains:[
+            'images.pexels.com',
+            'dummyimage.com'
+        ]
+    }
+};
+
+export default nextConfig;
+
--- src/app/layout.js
+++ src/app/layout.js
@@ -15,24 +15,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <header>
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content="
-              default-src 'none';
-              script-src 'self' 'nonce-random123' https://trusted.cdn.com;
-              style-src 'self' 'unsafe-inline';
-              font-src 'self' https://fonts.example.com; // Permitir fuentes
-              img-src 'self' data: https://images.example.com;
-              connect-src 'self' https://api.example.com;
-              media-src 'self' https://media.example.com; // Permitir medios
-              frame-src https://www.example.com; // Permitir enmarcar ciertos sitios
-              frame-ancestors 'none';
-              base-uri 'self';
-              form-action 'self';
-            "
-          />
-        </header>
       <body className={inter.className}>
         <Navbar />
         <SocialMediaBar />
