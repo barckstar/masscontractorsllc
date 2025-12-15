f06235fb9433c03d7ecf651a578e46a5311dd1d1
BarckStar <BarckStar@DESKTOP-NQARE9S>
2025-12-15 17:49:58 -0600

    feat: Add initial root layout with global components, SEO metadata, and structured data.

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,6 +36,7 @@ export default function RootLayout({ children }) {
       "postalCode": "23236",
       "addressCountry": "US"
     },
+    "priceRange": "$$",
     "openingHoursSpecification": {
       "@type": "OpeningHoursSpecification",
       "dayOfWeek": [
